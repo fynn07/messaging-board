090b0d385eb4c7f753345f89faccd0759627010a
Fynn Borja <pewdiestuff@gmail.com>
2024-05-03 23:17:12 +0800

    Fixed NO CORS

--- server/index.js
+++ server/index.js
@@ -6,11 +6,7 @@ const server = require('http').createServer(app);
 const PORT = 3200;
 
 
-app.use(cors({
-    origin: ['http://localhost:3200', 'https://messaging-board-nine.vercel.app'],
-    credentials: true,
-    exposedHeaders: ["Authorization"]
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/', require('./routes/mainRoutes'));
