009343fba1684a1d748b4f1de3cce9ac37241108
Fynn Borja <pewdiestuff@gmail.com>
2024-05-03 23:22:54 +0800

    Fixed Issues

--- server/index.js
+++ server/index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config()
 const app = express();
-const server = require('http').createServer(app);
 const PORT = 3200;
-
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/', require('./routes/mainRoutes'));
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Running server");
 })
