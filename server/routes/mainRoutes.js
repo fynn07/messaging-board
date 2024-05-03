275ab293e30d5c614850f062b69ef5b4a8f10b95
Fynn Borja <pewdiestuff@gmail.com>
2024-05-03 23:55:29 +0800

    fixed server

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const { postChat, getAllChat } = require("../controller/chatController");
-const { rateLimiterMiddleware } = require("../config/rateLimiter");
 const router = express.Router();
 
 router.route('/chat').get(getAllChat);
-router.post('/chat', rateLimiterMiddleware);
 router.route('/chat').post(postChat);
 
 module.exports = router
