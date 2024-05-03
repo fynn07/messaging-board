d6802077fae80ca7af6afc1a60978be1b73fd1da
Fynn Borja <pewdiestuff@gmail.com>
2024-05-03 22:11:22 +0800

    [SERVER] Added Rate Limiter

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config()
 const app = express();
+const rateLimit = require('express-rate-limit');
 const server = require('http').createServer(app);
 const PORT = 3200;
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/', require('./routes/mainRoutes'));
 
 server.listen(PORT, () => {
--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const { postChat, getAllChat } = require("../controller/chatController");
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
+
+const postRateLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, 
+    max: 10, 
+    standardHeaders: true, 
+    legacyHeaders: false, 
+});
 
 router.route('/chat').get(getAllChat);
-router.route('/chat').post(postChat);
+router.route('/chat').post(postRateLimiter, postChat);
 
 module.exports = router
