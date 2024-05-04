980b03665dbc75967c62640adfb41c48275b8a27
Fynn Borja <pewdiestuff@gmail.com>
2024-05-04 11:20:11 +0800

    [FIXED] Issues

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { postChat, getAllChat } = require("../controller/chatController");
-const rateLimiter = require("../config/rateLimiter")
+// const rateLimiter = require("../config/rateLimiter")
 const router = express.Router();
 
 router.route('/chat').post(postChat);
